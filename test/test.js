59438ba4d27fe9c0ee2a93cec1cede5c8b5fb317
Equan Pr <equan.p@gmail.com>
2015-10-04 05:33:31 +0700

    Show error when image file is not exist

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
 var img2lcd = require('../img2lcd.js');
 
 img2lcd.convert('./test/images/robot.png', function(err, hex){
-    if(!err) {
-      console.log(hex);
-    }
+    err ? console.log(err): console.log(hex);
 })
